07cb6b7bfe06aa107693b4ccb2c4cbc7612f7003
kazemawada <kazemawada1234@gmail.com>
2024-04-12 14:30:20 +0300

    2darkmode

--- shopmix/lib/designs/colors_design.dart
+++ shopmix/lib/designs/colors_design.dart
@@ -1,71 +1,25 @@
-
-
 import 'package:flutter/material.dart';
 
 class ColorsDesign {
-
-
   ColorsDesign();
 
-    bool isDark=false;
+  bool isDark = false;
 
+  void changeMode() {
+    isDark = !isDark;
+  }
 
-   final List<Color> darkMode=[Color(0xFFF44336)];
-   final List<Color> lightMode=[Color(0xFFF44336)];
+  final Map light = {
+    "input": Color(0xFFFFFF),
+    "label": Color(0x9B9B9B),
+    "background": Color(0xF9F9F9),
+    "botton": Color(0xDB3022)
+  };
 
-
-
-
-   void changeMode(){
-    isDark=!isDark;
-   }
-
-
-  int findCurrentLightModeColorIndex(Color color){
-
-    int index=lightMode.indexOf(color);
-    return index;
-
-   }
-
-  int findCurrentDarkModeColorIndex(Color color){
-
-    int index=darkMode.indexOf(color);
-    return index;
-
-   }
-
-//kazem
-
-
-   final Map light ={
-
-    "input":Color(0xFFFFFF) ,
-    "label":Color(0x9B9B9B),
-    "background":Color(0xF9F9F9),
-    "botton":Color(0xDB3022)
-
-    
-   };
-   
-   final Map dark ={
-
-    "input":Color( 0xFFFFFF) ,
-    "label":Color(0xC0C0C0),
-    "background":Color(0x1A1A1A),
-    "botton":Color(0xDB3022)
-
-    
-   };
-
-
-
-
-
-
-
-
-
-
-
+  final Map dark = {
+    "input": Color(0xFFFFFF),
+    "label": Color(0xC0C0C0),
+    "background": Color(0x1A1A1A),
+    "botton": Color(0xDB3022),
+  };
 }
